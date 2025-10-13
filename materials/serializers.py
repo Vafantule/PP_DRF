d8e724ffc42446a473c01c26d6983f8333a3e21d
Vafantule <pavelgav@gmail.com>
2025-10-13 18:18:05 +0500

    add serializer for app materials

---
+++ materials/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Course, Lesson
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор курса.
+    """
+    class Meta:
+        model = Course
+        fields = "__all__"
+
+
+class LessonSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор урока.
+    """
+    class Meta:
+        model = Lesson
+        fields = "__all__"
