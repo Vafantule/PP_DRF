65470cf156f7b022c243fe7240fa3562108f4c87
Vafantule <pavelgav@gmail.com>
2025-10-13 18:12:20 +0500

    add model.lesson for app materials

--- materials/models.py
+++ materials/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Course(models.Model)
+class Course(models.Model):
     """
     Модель курса.
     """
@@ -13,5 +13,23 @@ class Course(models.Model)
         verbose_name = "Курс"
         verbose_name_plural = "Курсы"
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
+
+
+class Lesson(models.Model):
+    """
+    Модель урока, связанная с курсом.
+    """
+    title = models.CharField(max_length=180, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    preview = models.ImageField(upload_to="courses/previews", verbose_name="Превью", blank=True, null=True)
+    video_url = models.URLField(verbose_name="Ссылка на видео")
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="lessons", verbose_name="Курс")
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+
+    def __str__(self) -> str:
+        return f"{self.title} ({self.course.title})"
