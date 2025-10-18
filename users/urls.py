1820411ed85d398746210050743e077427ddfbf4
Vafantule <pavelgav@gmail.com>
2025-10-18 15:04:46 +0500

    add users.urls for payment url

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
-from .views import UserRegistrationAPIView
+from django.urls import path, include
+from .apps import UsersConfig
+from .views import UserRegistrationAPIView, PaymentViewSet
+from rest_framework.routers import DefaultRouter
+
+
+app_name = UsersConfig.name
+
+router = DefaultRouter()
+router.register(r"payments", PaymentViewSet, basename="payments")
+
 
 urlpatterns = [
     path('register/', UserRegistrationAPIView.as_view(), name='user-register'),
-]
+] + router.urls
