f9d653c5e6f69863444461475ef864fd15b4e4e7
Vafantule <pavelgav@gmail.com>
2025-10-18 16:50:20 +0500

    change materials.serialisers courseserialiser

--- materials/serializers.py
+++ materials/serializers.py
@@ -2,11 +2,21 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор урока.
+    """
+    class Meta:
+        model = Lesson
+        fields = "__all__"
+
+
 class CourseSerializer(serializers.ModelSerializer):
     """
     Сериализатор курса.
     """
     lessons_count = serializers.SerializerMethodField(read_only=True)
+    lessons = LessonSerializer(many=True, read_only=True, source="lessons")
 
     class Meta:
         model = Course
@@ -15,11 +25,3 @@ class CourseSerializer(serializers.ModelSerializer):
     def get_lessons_count(self, obj: Course) -> int:
         return obj.lessons.count()
 
-
-class LessonSerializer(serializers.ModelSerializer):
-    """
-    Сериализатор урока.
-    """
-    class Meta:
-        model = Lesson
-        fields = "__all__"
