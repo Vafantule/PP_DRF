e2cd5c61e5594c7c53ed1def83919ff5c35afc0b
Vafantule <pavelgav@gmail.com>
2025-10-18 14:57:20 +0500

    change users.views payment controller for permission

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, viewsets, permissions
 from rest_framework.permissions import AllowAny
+from rest_framework.request import Request
 
 from .models import Payment
 from .serializers import UserRegistrationSerializer, PaymentSerializer
@@ -19,3 +20,17 @@ class PaymentViewSet(viewsets.ModelViewSet):
     """
     queryset = Payment.objects.all().select_relater("user", "paid_course", "paid_lesson")
     serializer_class = PaymentSerializer
+
+    def get_permissions(self) -> list[permissions.BasePermission]:
+        if self.request.method in ("GET", "HEAD", "OPTIONS"):
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
+
+    def perform_create(self, serializer: PaymentSerializer) -> None:
+        user = serializer.validated_data("user")
+        if user is None and (isinstance(self.request, Request) and
+                             self.request.user and
+                             self.request.user.is_authenticated):
+            serializer.save(user=self.request.user)
+        else:
+            serializer.save()
