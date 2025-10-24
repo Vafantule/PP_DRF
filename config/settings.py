7e532b4eb8177abf7de79f2b4b9ae3aa020ab545
Vafantule <pavelgav@gmail.com>
2025-10-24 18:06:52 +0500

    change project urls & settings

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         "rest_framework.authentication.SessionAuthentication",
