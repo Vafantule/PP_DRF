81e3cfa6b877205795e719797db61affb7e50b58
Vafantule <pavelgav@gmail.com>
2025-10-29 13:01:28 +0500

    add new model subscription

--- materials/models.py
+++ materials/models.py
@@ -50,3 +50,25 @@ class Lesson(models.Model):
 
     def __str__(self) -> str:
         return f"{self.title} ({self.course.title})"
+
+
+class Subscription(models.Model):
+    """
+    Модель пользователя на обновления курса.
+    """
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="course_subscriptions",
+        verbose_name="Пользователь",
+    )
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="subscriptions", verbose_name="Курс")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата подписки")
+
+    class Meta:
+        verbose_name = "Подписка на курс"
+        verbose_name_plural = "Подписки на курс"
+        constraints = [models.UniqueConstraint(fields=["user", "course"], name="unique_user_course_subscription")]
+
+    def __str__(self):
+        return f"Подписка (пользователь={self.user_id}, курс={self.course_id})"
