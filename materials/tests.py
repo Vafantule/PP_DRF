ed102860c4a652dff962c86bd08297af146ea02b
Vafantule <pavelgav@gmail.com>
2025-10-31 17:35:29 +0500

    fix test for lesson owner can delete own lesson after testing

--- materials/tests.py
+++ materials/tests.py
@@ -187,12 +187,12 @@ class LessonAPITest(APITestCase):
         temp = Lesson.objects.create(
             title="Название урока",
             description="Описание урока",
-            сourse=self.course,
+            course=self.course,
             owner=self.owner,
             video_url="https://youtube.com/1"
         )
         self.auth_as(self.owner)
         response_custom = self.client.delete(self.lesson_delete_url(temp.id))
         self.assertIn(response_custom.status_code, (status.HTTP_204_NO_CONTENT, status.HTTP_200_OK))
-        self.assertFalse(Lesson.objects.filter(pk=temp.id).exists(course=self.course))
+        self.assertFalse(Lesson.objects.filter(pk=temp.id).exists())
 
